c3f6177b412c2eb9e3a55a4846515e468fab6905
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-01-24 20:36:28 +0530

    Initializing the login system

---
+++ quizes/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import random
+
+DIFF_CHOICES = (   
+    ('easy','easy'),
+    ('medium','medium'),
+    ('hard','hard'),
+)
+# Create your models here.
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="Duration of the quiz")
+    required_score_to_pass = models.IntegerField(help_text="required score in percent")
+    difficulty = models.CharField(max_length=6,choices=DIFF_CHOICES)
+
+    class Meta:
+        verbose_name_plural = 'Quizes'
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_question(self):
+        que = list(self.question_set.all())
+        print(que)
+        random.shuffle(que)
+        return que[:self.number_of_questions] 
