ac139340bfd4e63e8c1be918e32e5b22c5cf288c
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-01-29 00:27:00 +0530

    adding the ajax request

--- quizes/static/quizes/add_quiz.js
+++ quizes/static/quizes/add_quiz.js
@@ -1,7 +1,43 @@
 console.log("Add_quiz")
 
+const addForm = document.getElementById('addForm')
+const submit = document.querySelector('#submit')
+const question_answer = document.getElementById('que-ans')
 
+const sendData = () => {
+const url = window.location.href
+const amount = document.getElementById('num_of_question')
+const difficulty = document.querySelector('#difficult')
+const choice = document.querySelector('#cho')
+const data = {}
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+data['csrfmiddlewaretoken'] = csrf[0].value
+data['amount'] = amount.value
+data['difficulty'] = difficulty.value
+data['choice'] = choice.value
+console.log(data['choice'])
+data['type'] = "multiple"
+console.log(data)
 
 $.ajax({
+    type:'POST',
+    url:`${url}`,
+    data:data,
+    success:function(response){
+        addForm.classList.add('not-visible')
+        const result = response.data
+        console.log(result)
+    },
+    error:function(response){
+        console.log(response)
+    }
+})
 
+}
+
+
+addForm.addEventListener('submit',e=>{
+    e.preventDefault()
+    sendData()
+    
 })
