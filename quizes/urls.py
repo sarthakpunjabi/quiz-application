ce301e6a1b469e22d2b1f1435d9213e455239ea0
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-01-28 08:05:57 +0530

    Adding add_quiz path url

--- quizes/urls.py
+++ quizes/urls.py
@@ -3,14 +3,16 @@ from .views import (
     QuizListView,
     quiz_view,
     quiz_data_view,
-    save_quiz_view
+    save_quiz_view,
+    add_quiz
 )
 
 app_name = 'quizes'
 
 urlpatterns = [
     path('',QuizListView.as_view(),name='main-view'),
+    path('add/',add_quiz,name='add-quiz'),
     path('<pk>/',quiz_view,name='quiz-view'),
     path('<pk>/data',quiz_data_view,name="quiz-data-view"),
-    path('<pk>/save',save_quiz_view,name="save-view")
+    path('<pk>/save',save_quiz_view,name="save-view"),
 ]
