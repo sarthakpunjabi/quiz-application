86b548692622a9a9b75f6f407977dd8a240ebfe4
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-01-30 01:48:11 +0530

    adding the logic to hide form and display the question and answers with the correct one

--- quizes/static/quizes/add_quiz.js
+++ quizes/static/quizes/add_quiz.js
@@ -3,6 +3,10 @@ console.log("Add_quiz")
 const addForm = document.getElementById('addForm')
 const submit = document.querySelector('#submit')
 const question_answer = document.getElementById('que-ans')
+const quizBox = document.getElementById('quiz-box')
+const quiForm = document.getElementById('quiz-form')
+
+
 
 const sendData = () => {
 const url = window.location.href
@@ -23,16 +27,39 @@ $.ajax({
     data:data,
     success:function(response){
         addForm.classList.add('not-visible')
-        const data = response.data
-        data.forEach(el => {
-            for (const [question,incorrect_answers,correct_answer] of Object.entries(el)){
-                console.log(question)
-                console.log(incorrect_answers)
-                console.log(correct_answer)
-            }
-        });
+        quiForm.classList.remove('not-visible')
         
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+                quizBox.innerHTML += `
+                    <hr>
+                    <div class="mb-2">
+                        <b>${el.question}</b>
+                    </div>
+                    
+                `
+                el.incorrect_answers.forEach(answer=>{
+                    quizBox.innerHTML += `
+                        <div>
+                            <input type='radio' class='ans' id='${el.question}-${answer}' name='${el.question}' value='${answer}' disabled>
+                            <label for='${el.question}'>${answer}</label>
+                        </div>
+                        
+                    `
+                })
+
+                quizBox.innerHTML += `
+                <div>
+                    <input type='radio' class='ans' id='${el.question}-${el.correct_answer}' name='${el.question}' value='${el.correct_answer}' checked>
+                    <label for='${el.question}'>${el.correct_answer}</label>
+                </div>
+                <br>
+                `
+            
+        })
     },
+
     error:function(response){
         console.log(response)
     }
