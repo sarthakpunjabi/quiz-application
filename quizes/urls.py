c1404866e3b0a1e1d161baa0a4c687fcfeb09709
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-02-01 18:33:13 +0530

    Making post route for saving the quiz

--- quizes/urls.py
+++ quizes/urls.py
@@ -4,7 +4,8 @@ from .views import (
     quiz_view,
     quiz_data_view,
     save_quiz_view,
-    add_quiz
+    add_quiz,
+    add_save
 )
 
 app_name = 'quizes'
@@ -12,6 +13,7 @@ app_name = 'quizes'
 urlpatterns = [
     path('',QuizListView.as_view(),name='main-view'),
     path('add/',add_quiz,name='add-quiz'),
+    path('add/save',add_save,name='add-save'),
     path('<pk>/',quiz_view,name='quiz-view'),
     path('<pk>/data',quiz_data_view,name="quiz-data-view"),
     path('<pk>/save',save_quiz_view,name="save-view"),
